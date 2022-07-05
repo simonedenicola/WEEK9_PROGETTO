9bd88dad0a31b031c364ab242a6beb2fb3aa9fe2
Simone De_Nicola <simonegiurisprudenza@gmail.com>
2022-07-05 15:35:37 +0200

    lastcommit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
 "use strict";
-class Smartphone {
+class User {
+    constructor(_credito, _numerochiamate) {
+        this.credito = _credito;
+        this.numerochiamate = _numerochiamate;
+    }
+    ricarica(unaRicarica) {
+        this.credito += unaRicarica;
+    }
+    chiamata(minutiDurata) {
+        let tariffa = 0.20;
+        this.credito = this.credito - (tariffa * minutiDurata);
+        this.numerochiamate++;
+    }
+    numero404() {
+        return this.credito;
+    }
+    getNumeroChiamate() {
+        return this.numerochiamate;
+    }
+    azzeraChiamate() {
+        this.numerochiamate = 0;
+    }
 }
+let ClienteUno = new User(1, 2);
+ClienteUno.ricarica(10);
+ClienteUno.chiamata(5);
+let ClienteDue = new User(3, 4);
+ClienteDue.ricarica(20);
+ClienteDue.chiamata(10);
+let ClienteTre = new User(5, 6);
+ClienteTre.ricarica(30);
+ClienteTre.chiamata(15);
+console.log("---PRIMO UTENTE---");
+console.log("Chiamate Effettuate" + ClienteUno.getNumeroChiamate());
+console.log("Credito Disponibile" + ClienteUno.numero404());
+ClienteUno.azzeraChiamate();
+console.log("Dopo l'azzeramento delle chiamate: " + ClienteUno.getNumeroChiamate());
+ClienteUno.azzeraChiamate();
+console.log("---SECONDO UTENTE---");
+console.log("Chiamate Effettuate" + ClienteDue.getNumeroChiamate());
+console.log("Credito Disponibile" + ClienteDue.numero404());
+ClienteDue.azzeraChiamate();
+console.log("Dopo l'azzeramento delle chiamate: " + ClienteDue.getNumeroChiamate());
+console.log("---TERZO UTENTE---");
+console.log("Chiamate Effettuate" + ClienteTre.getNumeroChiamate());
+console.log("Credito Disponibile" + ClienteTre.numero404());
+ClienteTre.azzeraChiamate();
+console.log("Dopo l'azzeramento delle chiamate: " + ClienteTre.getNumeroChiamate());
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,95 @@
-class Smartphone{
-   
+// Interfaccia smartphone
+interface Smartphone {
+
+    credito: number;
+    numerochiamate: number;
+
+    ricarica(credito: number, unaRicarica: number): void;
+    chiamata(credito: number, minutiRicarica: number): void;
+    numero404(): number;
+    getNumeroChiamate(): number;
+    azzeraChiamate():void;
 }
+
+
+class User implements Smartphone {
+
+    
+    public numerochiamate: number;
+    public credito: number;
+
+    
+    constructor(_credito: number, _numerochiamate:  number) {
+        this.credito = _credito;
+        this.numerochiamate = _numerochiamate;
+    } 
+
+
+
+    
+    public ricarica( unaRicarica: number):void {
+        this.credito += unaRicarica;
+    }
+
+    
+    public chiamata( minutiDurata: number):void {
+        let tariffa = 0.20;
+        this.credito= this.credito-(tariffa * minutiDurata);
+        this.numerochiamate++;
+    }
+
+    
+    public numero404():number{
+        return this.credito;
+    }
+
+    
+    public getNumeroChiamate():number{
+        return this.numerochiamate;
+    }
+
+    
+    public azzeraChiamate():void{
+        this.numerochiamate=0;
+    }
+}
+
+let ClienteUno = new User(1, 2);
+ClienteUno.ricarica(10);
+ClienteUno.chiamata(5);
+
+let ClienteDue = new User(3, 4);
+ClienteDue.ricarica(20);
+ClienteDue.chiamata(10);
+
+let ClienteTre = new User(5, 6);
+ClienteTre.ricarica(30);
+ClienteTre.chiamata(15);
+
+
+    console.log("---PRIMO UTENTE---")
+
+    console.log("Chiamate Effettuate"+ClienteUno.getNumeroChiamate());
+    console.log("Credito Disponibile"+ClienteUno.numero404());
+    ClienteUno.azzeraChiamate();
+    console.log("Dopo l'azzeramento delle chiamate: " + ClienteUno.getNumeroChiamate());
+
+    ClienteUno.azzeraChiamate();
+
+
+    console.log("---SECONDO UTENTE---")
+
+    console.log("Chiamate Effettuate"+ClienteDue.getNumeroChiamate());
+    console.log("Credito Disponibile"+ClienteDue.numero404());
+    ClienteDue.azzeraChiamate();
+    console.log("Dopo l'azzeramento delle chiamate: " + ClienteDue.getNumeroChiamate());
+
+
+    
+    console.log("---TERZO UTENTE---")
+
+    console.log("Chiamate Effettuate"+ClienteTre.getNumeroChiamate());
+    console.log("Credito Disponibile"+ClienteTre.numero404());
+    ClienteTre.azzeraChiamate();
+    console.log("Dopo l'azzeramento delle chiamate: " + ClienteTre.getNumeroChiamate());
+ 
